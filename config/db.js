dcc3786f012a202d6ad51e66020cf3d4519ee89c
y2Dev <dub-y@tuta.io>
2022-10-11 10:33:06 -0400

    mise en place de la connexion à la base de donnée MongoBD

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://" + process.env.BD_USER_PASS + "@cluster0.l2bpyiq.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require('dotenv').config({path: './config/.env'}) // configuration du chemin pour aller vers les variables d'environnement
+require('./config/db')
 const app = express();
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port 5000 ${process.env.PORT}`);
+    console.log(`Listening on port : ${process.env.PORT}`);
 })
