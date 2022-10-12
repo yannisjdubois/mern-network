852671661f928d9ba6b0016cae5654edbd43ac09
y2Dev <dub-y@tuta.io>
2022-10-12 12:01:56 -0400

    création de la méthode Delete pour effacer les informations liées à l'identifiant sélectionné

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,7 +10,7 @@ module.exports.getAllUsers = async (req, res) => {
 // Chercher les informations d'un seul utilisateur
 module.exports.userInfo = (req, res) => {
     console.log(req.params);
-    if (!ObjectID.isValid(req.params.id)) // Si ObjectID qui appelle la fonction isValid ne trouve pas l'identifiant recherché, ...
+    if (!ObjectID.isValid(req.params.id)) // Si ObjectID qui appelle la méthode isValid ne trouve pas l'identifiant recherché, ...
     return res.status(400).send('ID unknown :' + req.params.id) // ..., retourne status 400 + envoie message
     
     UserModel.findById(req.params.id, (err, docs) => {
@@ -22,7 +22,7 @@ module.exports.userInfo = (req, res) => {
 
 // Modifier les informations d'utilisateurs
 module.exports.updateUser = async(req, res) => {
-    if (!ObjectID.isValid(req.params.id)) // Si ObjectID qui appelle la fonction isValid ne trouve pas l'identifiant recherché, ...
+    if (!ObjectID.isValid(req.params.id)) // Si ObjectID qui appelle la méthode isValid ne trouve pas l'identifiant recherché, ...
     return res.status(400).send('ID unknown :' + req.params.id) // ..., retourne status 400 + envoie message
     
     try {
@@ -39,6 +39,18 @@ module.exports.updateUser = async(req, res) => {
                 if (err) return res.status(500).send({ message: err });
             }
         )
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+};
+
+module.exports.deleteUser = async(req, res) => {
+    if (!ObjectID.isValid(req.params.id)) // Si ObjectID qui appelle la méthode isValid ne trouve pas l'identifiant recherché, ...
+    return res.status(400).send('ID unknown :' + req.params.id) // ..., retourne status 400 + envoie message
+    
+    try {
+        await UserModel.remove({ _id: req.params.id }).exec();
+        res.status(200).json({ message: "Successfully deleted."});
     } catch (err) {
         return res.status(500).json({ message: err });
     }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,5 +9,6 @@ router.post("/register", authController.signUp);
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo); // quand j'ai le paramètre /:id, j'active la fonction userInfo contenue dans userController 
 router.put('/:id', userController.updateUser); 
+router.delete('/:id', userController.deleteUser); 
 
 module.exports = router;
