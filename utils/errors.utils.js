176c9a11a58a0ff04b71a6c9af8c0576f078ab15
y2Dev <dub-y@tuta.io>
2022-10-17 10:32:32 -0400

    fonction différenciant d'où provient le code d'erreur 11000

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -10,7 +10,10 @@ module.exports.signUpErrors = (err) => {
     if (err.message.includes('password'))
         errors.password = "Le mot de passe doit faire 6 caractères minimum";
 
-    if (err.code === 11000)
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('pseudo')) // permet d'identifier quand le champs pseudo contient une valeur déjà présente en base de données
+        errors.pseudo = 'Ce pseudo est déjà pris';
+
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email')) // permet d'identifier quand le champs email contient une valeur déjà présente en base de données
         errors.email = 'Cet email est déjà enregistré';
 
     return errors
