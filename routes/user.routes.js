c01bebb0fcb9a306dc26b946dabd654d3d6a60fd
y2Dev <dub-y@tuta.io>
2022-10-18 11:27:25 -0400

    installation du paquet Multer afin de traiter les téléchargements d'images

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,19 +1,23 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
+const uploadController = require('../controllers/upload.controller');
 
-// auth
+// Auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logout);
 
 
-// user display: 'block',
+// User display: 'block',
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo); // quand j'ai le paramètre /:id, j'active la fonction userInfo contenue dans userController 
 router.put('/:id', userController.updateUser); 
 router.delete('/:id', userController.deleteUser); 
 router.patch('/follow/:id', userController.follow);
 router.patch('/unfollow/:id', userController.unfollow);
+
+// Upload
+router.post('/upload', uploadController.uploadProfil)
 
 module.exports = router;
