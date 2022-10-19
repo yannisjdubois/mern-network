111ce1508d1396920f05f33ec59d3fbaa7f4b1b2
y2Dev <dub-y@tuta.io>
2022-10-19 11:24:36 -0400

    création d'une fonction qui regroupe les messages d'erreurs lors d'un téléchargement

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/user.model');
 const fs = require('fs');
 const { promisify } = require('util');
+const { uploadErrors } = require('../utils/errors.utils');
 const pipeline = promisify(require('stream').pipeline);
 
 module.exports.uploadProfil = async (req, res) => {
@@ -10,11 +11,12 @@ module.exports.uploadProfil = async (req, res) => {
             req.file.detectedMimeType != "image/png" &&
             req.file.detectedMimeType != "image/jpeg"
             )
-            throw Error("invalid"); // throw arrête le try et lance le catch
+            throw Error("invalid file"); // throw arrête le try et lance le catch
 
         if (req.file.size > 500000) throw Error("max size");  // throw arrête le try et lance le catch
     } catch (err) {
-        return res.status(201).json(err);
+        const errors = uploadErrors(err)
+        return res.status(201).json({ errors });
     }
 
     const fileName = req.body.name + ".jpg"; // chaque nouvelle photo écrasera la précédente
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -1,3 +1,4 @@
+// Les messages d'erreurs lors de l'inscription
 module.exports.signUpErrors = (err) => {
     let errors = { pseudo: '', email: '', password: 'mauvais password'}
 
@@ -19,14 +20,26 @@ module.exports.signUpErrors = (err) => {
     return errors;
 };
 
+// Les messages d'erreurs lors de la connexion
 module.exports.signInErrors = (err) => {
     let errors = { email: '', password: ''}
 
     if (err.message.includes('email'))
     errors.email = "Email inconnu";
 
-if (err.message.includes('password'))
+    if (err.message.includes('password'))
     errors.password = "Le mot de passe ne correspond pas";
 
     return errors;
+}
+
+// Les messages d'erreurs lors d'un téléchargement
+module.exports.uploadErrors = (err) => {
+    let errors = { format: '', maxSize: ""};
+
+    if (err.message.includes('invalid file'))
+    errors.format = "Format incompatible";
+
+    if (err.message.includes('max size'))
+    errors.maxSize = "Le fichier dépasse 500ko";
 }
